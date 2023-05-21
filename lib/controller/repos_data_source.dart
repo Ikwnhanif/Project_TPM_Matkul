46f723d513f20049b854c0234ad33a5b474d4032
ikwnhanif <hikwan7@gmail.com>
2023-05-22 06:28:24 +0700

    Update comments

--- lib/controller/repos_data_source.dart
+++ lib/controller/repos_data_source.dart
@@ -1,9 +1,5 @@
 import '../service/base_network_repo.dart';
 
-//Dengan memanggil method loadBooks(),  mengembalikan nilai dari class
-//BaseNetwork dengan method get() diisi dengan parameter “text” dikarenakan ingin
-//mengambil list dari user.
-
 class RepoDataSource {
   static RepoDataSource instance = RepoDataSource();
   Future<Map<String, dynamic>> loadRepos(String text) {
--- lib/controller/users_data_source.dart
+++ lib/controller/users_data_source.dart
@@ -1,9 +1,5 @@
 import '../service/base_network.dart';
 
-//Dengan memanggil method loadBooks(),  mengembalikan nilai dari class
-//BaseNetwork dengan method get() diisi dengan parameter “text” dikarenakan ingin
-//mengambil list dari user.
-
 class UserDataSource {
   static UserDataSource instance = UserDataSource();
   Future<Map<String, dynamic>> loadUsers(String text) {
